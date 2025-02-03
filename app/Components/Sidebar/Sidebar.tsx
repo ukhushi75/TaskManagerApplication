2d82dbef71e3c988b2635fc4e8c497369e8902b6
Khushi <ukhushi75@gmail.com>
2025-02-02 19:11:01 -0600

    code updated

--- app/Components/Sidebar/Sidebar.tsx
+++ app/Components/Sidebar/Sidebar.tsx
@@ -1,8 +1,13 @@
 "use client"
 import React from "react";
 import styled from "styled-components";
+import { useGlobalState } from "@/app/context/globalProvider";
 
 function Sidebar(){
+    const context = useGlobalState();
+
+    console.log(context);
+
     return(
         <SidebarStyled>Sidebar</SidebarStyled>
     )
--- app/context/globalProvider.js
+++ app/context/globalProvider.js
@@ -1,24 +1,24 @@
 "use client"
-//here we're going to create the context for updating and the actual context
-//input create context
+
 
 import React, { createContext, useState, useContext} from "react"
 import { Children } from "react"
 import themes from "./themes"
 export const GlobalContext = createContext()
 export const GlobalUpdateContext = createContext()
-//its going to be our entire application so we're going to wrap with the global provider
+
 export const GlobalProvider = ({ Children}) => {
-//create a state for the theme
     
 const [selectedTheme, setSelectedTheme] = useState(0);
 const theme = themes[0];
-//theme = an array and the selection is going to be 0
     return (
-        <GlobalContext.Provider value={{theme, }}>
+        <GlobalContext.Provider value={{}}>
             <GlobalUpdateContext.Provider value={setGlobalState}>
                 {Children}
             </GlobalUpdateContext.Provider>
         </GlobalContext.Provider>
     );
 };
+
+export const useGlobalState = () => useContext(GlobalContext);
+export const useGlobalUpdate = () => useContext(GlobalUpdateContext);
